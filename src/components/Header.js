9093e41adea8d8c13c9c9921b71dfedd2f720d4b
Alex Greenberg <acgreenberg2@gmail.com>
2024-01-25 20:28:05 -0500

    finished up the Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// the component function
+const Header = (props) => {
+
+    return (
+    <div className="nav-container"> 
+        <h1>Alex Greenberg's Portfolio</h1>
+        <div className="links-container">
+            <a href="https://github.com/a1g23">GitHub</a>
+            <a href="https://www.linkedin.com/in/alex-greenberg567/">LinkedIn</a>
+        </div>
+    </div>
+    )
+}
+
+// export the component
+export default Header
